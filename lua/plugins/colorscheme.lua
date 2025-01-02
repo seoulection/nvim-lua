bcbd9e905bd6347f83f422ec05a2096148e0504f
seoulection <6445410+seoulection@users.noreply.github.com>
2025-01-02 07:49:55 -0800

    adds new colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
-	"junegunn/seoul256.vim",
-	priority = 1000, -- make sure to load this before all the other plugins
-	init = function()
-		vim.cmd.colorscheme("seoul256")
-		vim.cmd.hi("Comment gui=none")
-	end,
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("github-theme").setup({
+				-- ...
+			})
+
+			vim.cmd("colorscheme github_dark")
+		end,
+	},
 }
