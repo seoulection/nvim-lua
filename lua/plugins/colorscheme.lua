87bf00c92a5c52215d01f35c43a41b391904d923
seoulection <6445410+seoulection@users.noreply.github.com>
2025-03-30 13:54:30 -0700

    adds new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
 	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
+		"savq/melange-nvim",
+		name = "melange",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("github-theme").setup({
-				-- ...
-			})
+			--require("melange").setup({
+			--	-- ...
+			--})
 
-			vim.cmd("colorscheme github_dark")
+			vim.opt.termguicolors = true
+			vim.cmd.colorscheme("melange")
 		end,
 	},
 }
